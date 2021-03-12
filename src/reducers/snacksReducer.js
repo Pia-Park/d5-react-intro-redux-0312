eb94a5bbe2e61675a5202dbaba360590b59ec452
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2021-03-12 11:52:55 -0800

    first commit

---
+++ src/reducers/snacksReducer.js
@@ -0,0 +1,28 @@
+const snacksData =[
+    {food:'Doritos', quantity: 10},
+    {food:'Jagabee', quantity: 6},
+    {food:'Haribo', quantity: 22},
+    {food:'Calbee', quantity: 12},
+    {food:'M&Ms', quantity: 2},
+]
+
+
+const snacksReducer = (state = snacksData, action) => {
+// console.log(action)
+if(action.type === 'UPDATE_SNACKS'){
+    const newState = [...state]; 
+
+    if(action.payload.operator === '+'){
+        newState[action.payload.index].quantity++
+    }else if(action.payload.operator === '-'){
+        newState[action.payload.index].quantity--
+    }
+
+    return newState
+}else{
+    return state
+}
+
+}
+
+export default snacksReducer;
